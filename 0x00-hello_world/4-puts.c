d17d8f53dbedda25b42b7e20f34d884564ee0542
tahiri-houda <tahirihouda731@gmail.com>
2023-03-16 10:33:16 -0700

    test

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print line with puts
+ * Return: 0 (approved)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
