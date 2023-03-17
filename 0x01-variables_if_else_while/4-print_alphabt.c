138ab2067b3ee171c51ca8b12a34ecc53cc165f4
tahiri-houda <tahirihouda731@gmail.com>
2023-03-17 16:33:56 -0700

    test

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - use if and while
+ *Return:0;
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
