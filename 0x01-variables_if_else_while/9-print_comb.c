4fca7ba5a874999c19ce894142ef0a2cfebe312e
tahiri-houda <tahirihouda731@gmail.com>
2023-03-17 18:01:18 -0700

    test

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print comb of numbers'
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(48 + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
