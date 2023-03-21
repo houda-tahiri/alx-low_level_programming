23348d08a0d3f98120cce06f0fd776e6a5e3b9ac
tahiri-houda <tahirihouda731@gmail.com>
2023-03-21 12:16:05 -0700

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a' ;  n <= 'z' ; n++)
+		_putchar(n);
+	_putchar('\n');
+}
